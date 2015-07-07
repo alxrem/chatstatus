3a216242f1e328abcf558e6e4e9ad6fb252adf77
Alexey Remizov <rem@onsec.ru>
2015-07-07 11:36:20 +0300

    Methods relating to Telepathy moved to separate object

--- extension.js
+++ extension.js
@@ -14,13 +14,89 @@ var button = null;
 var menu_actor = null;
 var timer_start = null;
 var timer_loop = null;
+var messenger = null;
+
+var Telepathy = {
+	contactListCommand: "empathy",
+
+	getStatus: function() {
+		var AccListArr = Telepathy._getAccountsList();
+		var inc = 0;
+		var AccName = "";
+		var LastStatus = "";
+
+		for (inc = 0; inc < AccListArr.length - 1 ; inc++) {
+			AccName = new String(AccListArr[inc]);
+
+			if(Telepathy._is_managable_account(AccName)) {
+				var AccStatus = new String(GLib.spawn_command_line_sync('mc-tool show ' + AccName.replace(/,/g,""))[1]);
+				LastStatus = new String(AccStatus.match(/Current:.*/));
+				LastStatus = LastStatus.replace(/Current:/,"");
+				LastStatus = LastStatus.replace(/\".*\"/,"");
+				LastStatus = LastStatus.replace(/\"/g, "");
+				LastStatus = LastStatus.replace(/[()0-9]/g, "");
+				LastStatus = LastStatus.trim();
+			}
+		}
+
+		if (! _arr_contains(KnownStatus,LastStatus)) {
+			LastStatus = "custom";
+		}
+		return LastStatus;
+	},
+
+	setStatus: function(requested_status) {
+		var AccListArr = Telepathy._getAccountsList();
+		var inc = 0;
+		var AccName;
+
+		for (inc = 0; inc < AccListArr.length - 1; inc++) {
+			AccName = new String(AccListArr[inc]);
+			AccName = AccName.replace(/,/g,"");
+
+			if(Telepathy._is_managable_account(AccName)) {
+				GLib.spawn_command_line_sync('mc-tool request ' + AccName + ' ' + requested_status);
+			}
+		}
+	},
+
+	_getAccountsList: function() {
+		var MCToolListShell = GLib.spawn_command_line_sync('mc-tool list');
+		var AccountListTxt = new String(MCToolListShell[1]);
+		var Ret = new Array();
+
+		Ret = AccountListTxt.split("\n");
+		return Ret;
+	},
+
+	_is_managable_account: function(accid) {
+		var is_managable = true;
+
+		//Enabled account?
+		var AccState = new String(GLib.spawn_command_line_sync('mc-tool show ' + accid.replace(/,/g,""))[1]);
+		var AccEnabled = new String(AccState.match(/Enabled:.*/));
+		AccEnabled = AccEnabled.replace(/Enabled: /,"");
+		AccEnabled = AccEnabled.trim();
+
+		if(accid.match(/\/irc\//) != null) {
+			is_managable = false;
+		}
+
+		if(AccEnabled.match(/enabled/) == null) {
+			is_managable = false;
+		}
+
+		return is_managable;
+	}
+}
 
 function enable() {
 	timer_start = Mainloop.timeout_add_seconds(REFRESH, function () {
+		messenger = Telepathy;
 		button = new PanelMenu.Button(0.0);
 		_set_panel_display(button);
 		_build_menu(button);
-	
+
 		Main.panel.addToStatusArea("chatstatus", button, 0, "right");
 		Mainloop.source_remove(timer_start);
 		timer_start = null;
@@ -57,7 +133,7 @@ function _set_panel_display(button,set_state = "") {
 	var final_status;
 
 	if (set_state == "") {
-		final_status = _getStatus();
+		final_status = messenger.getStatus();
 	}
 	else {
 		final_status = set_state;
@@ -70,14 +146,14 @@ function _set_panel_display(button,set_state = "") {
 }
 
 function _build_menu(button) {
-	if(_getStatus() != 'null') {
+	if(messenger.getStatus() != 'null') {
 		_add_item(button, Gettext.domain("chatstatus").gettext("Available"), 'available');
 		_add_item(button, Gettext.domain("chatstatus").gettext("Busy"), 'dnd');
 		_add_item(button, Gettext.domain("chatstatus").gettext("Away"), 'away');
 		_add_item(button, Gettext.domain("chatstatus").gettext("Invisible"), 'hidden');
 		_add_item(button, Gettext.domain("chatstatus").gettext("Offline"), 'offline');
 		_add_separator(button);
-		_add_item_run(button, Gettext.domain("chatstatus").gettext("Show contact list"), 'empathy');
+		_add_item_run(button, Gettext.domain("chatstatus").gettext("Show contact list"), messenger.contactListCommand);
 	}
 	else {
 		_add_item_run(button, Gettext.domain("chatstatus").gettext("Add new account"),'gnome-control-center online-accounts');
@@ -96,12 +172,12 @@ function _add_item(button, status_name, status_code) {
 				pack_start: false,
 				style_class: "chatstatus-menu-box"});
 	item.actor.add_child(box);
- 
+
 	let label = new St.Label({text: status_name});
 	box.add(label);
 	item.connect("activate", function () {
-		_setStatus(status_code);
-		_set_panel_display(button,status_code); 
+		messenger.setStatus(status_code);
+		_set_panel_display(button,status_code);
 	});
 }
 
@@ -112,85 +188,14 @@ function _add_item_run(button, label, command) {
 				pack_start: false,
 				style_class: "chatstatus-menu-box"});
 	item.actor.add_child(box);
- 
+
 	let label = new St.Label({text: label});
 	box.add(label);
-	
+
 	item.connect("activate", function () {
 		GLib.spawn_command_line_async(command);
 	});
 }
-
-function _getAccountsList() {
-	var MCToolListShell = GLib.spawn_command_line_sync('mc-tool list');
-	var AccountListTxt = new String(MCToolListShell[1]);
-	var Ret = new Array();
-
-	Ret = AccountListTxt.split("\n");
-	return Ret;
-}
-
-function _getStatus() {
-	var AccListArr = _getAccountsList();
-	var inc = 0;
-	var AccName = "";
-	var LastStatus = "";
-
-	for (inc = 0; inc < AccListArr.length - 1 ; inc++) {
-		AccName = new String(AccListArr[inc]);
-
-		if(_is_managable_account(AccName)) {     
-			var AccStatus = new String(GLib.spawn_command_line_sync('mc-tool show ' + AccName.replace(/,/g,""))[1]);
-			LastStatus = new String(AccStatus.match(/Current:.*/));
-			LastStatus = LastStatus.replace(/Current:/,"");
-			LastStatus = LastStatus.replace(/\".*\"/,"");
-			LastStatus = LastStatus.replace(/\"/g, "");
-			LastStatus = LastStatus.replace(/[()0-9]/g, "");
-			LastStatus = LastStatus.trim();
-		}
-	}
-
-	if (! _arr_contains(KnownStatus,LastStatus)) {
-		LastStatus = "custom";
-	}
-	return LastStatus;
-}
-
-function _setStatus(requested_status){
-	var AccListArr = _getAccountsList();
-	var inc = 0;
-	var AccName;
-
-	for (inc = 0; inc < AccListArr.length - 1; inc++) {
-		AccName = new String(AccListArr[inc]);
-		AccName = AccName.replace(/,/g,"");
-		
-		if(_is_managable_account(AccName)) {
-			GLib.spawn_command_line_sync('mc-tool request ' + AccName + ' ' + requested_status);
-		}
-	}
-}
-
-function _is_managable_account(accid) {
-	var is_managable = true;
-
-	//Enabled account?
-	var AccState = new String(GLib.spawn_command_line_sync('mc-tool show ' + accid.replace(/,/g,""))[1]);
-	var AccEnabled = new String(AccState.match(/Enabled:.*/));
-	AccEnabled = AccEnabled.replace(/Enabled: /,"");
-	AccEnabled = AccEnabled.trim();
-
-	if(accid.match(/\/irc\//) != null) { 
-		is_managable = false;		
-	}
-	
-	if(AccEnabled.match(/enabled/) == null) { 
-		is_managable = false;
-	}
-		
-	return is_managable;
-}
-
 
 function _arr_contains(a, obj) {
 	for (var i = 0; i < a.length; i++) {
